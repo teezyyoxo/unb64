8ba6cefd14853352178643e7df368d8ad1e527e0
Montel Gray <montel.gray@gmail.com>
2025-07-08 10:10:24 -0400

    Initial Commit

---
+++ unb64/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  unb64
+//
+//  Created by Montel Gray on 2025.07.08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ unb64/unb64App.swift
@@ -0,0 +1,17 @@
+//
+//  unb64App.swift
+//  unb64
+//
+//  Created by Montel Gray on 2025.07.08.
+//
+
+import SwiftUI
+
+@main
+struct unb64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
